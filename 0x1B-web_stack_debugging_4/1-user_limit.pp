7fe8a7b6ff56f03d3ff50073de58d8facea9f039
Yordi-Dereje <yordadereje@gmail.com>
2022-11-10 07:19:02 +0300

    1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,6 @@
-#Enable the user holberton to login and open files without errors
-
-# Increase hard file limit for holberton user
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
-}
-
-#Increase soft file limit for user holberton
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
-}
+#This script increases number of worker_processes
+exec { 'increase nginx worker processes':
+  command => "sed -i '$ i/* soft    nofile  1000' /etc/security/limits.conf;
+sed -i '$ i/* hard    nofile  100000' /etc/security/limits.conf;",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+  }
